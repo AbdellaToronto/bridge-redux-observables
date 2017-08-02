239ba49af35062f45c6cfd48d5a0c7af6cd2eb7a
Simone Holder <simone@simoneholder.com>
2017-08-02 19:34:22 -0400

    feature set up for recipe app

---
+++ src/redux/epics/recipe.epic.test.js
@@ -0,0 +1,34 @@
+import { ActionsObservable } from 'redux-observable';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+import { getRecipeByNameEpic } from './recipe.epics.js';
+import { RECIPE_ACTIONS } from '../actions/recipe.actions';
+
+
+// this creates an action observable, allowing us to fake an action that our epic can hear
+const action$ = ActionsObservable.of(
+  {type: RECIPE_ACTIONS.GET_RECIPES_BY_NAME, payload: 'chocolate'}
+);
+
+
+describe('getRecipeByName Epic', () => {
+
+  it('dispatches the correct action when it is successful', (done) => {
+    const ajax = () => Observable.of({ response: {
+      hits: [{recipe: 'Deep and Dellaicious Cake'}, {recipe: 'Mozzadella'}]
+    }}); // this will be replacing our ajax call!
+
+
+    getRecipeByNameEpic(action$, null, { ajax })
+      .subscribe(result => {
+        expect(result.type).toBe(RECIPE_ACTIONS.RECIPES_RECEIVED_SUCCESS);
+
+        expect(result.payload).toEqual(["Deep and Dellaicious Cake", "Mozzadella"]);
+
+        done();
+      })
+
+  });
+});
